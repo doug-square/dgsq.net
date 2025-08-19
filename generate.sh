b43c5bc0a0e007f0c05813ce2332fe408ceaa50e
dgsq <dgsq@dgsq.net>
2025-08-18 22:41:50 -0500

    Initial steps towards new website design.

---
+++ generate.sh
@@ -0,0 +1,28 @@
+#!/bin/bash    
+SRC="$(pwd)/src"
+OUT="$(pwd)/public"
+
+rm -rf "/${OUT}"
+mkdir -p "/${OUT}"
+
+cp -r "$SRC"/assets "$OUT"
+cp -r "$SRC"/root/* "$OUT"
+
+# shellcheck disable=SC2044
+for page in $(find "$SRC"/pages -type f)
+do
+  page_name=''${page#"$SRC"/pages}
+
+  if [[ $(dirname "$page_name") != "." ]]
+  then
+    mkdir -p "$OUT/$(dirname "$page_name")"
+  fi
+
+  {
+    cat "$SRC/pre.html"
+    cat "$page"
+    cat "$SRC/post.html"
+  } >> "$OUT/$page_name"
+done
+echo "generated site content from $SRC into $OUT"
+
